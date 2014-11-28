3b37ba6dbdcc5a95cfc7d51f3a2987ecb7a1c6f7
Joe Burns <josebur86@gmail.com>
2014-11-27 18:10:08 -0800

    Add the vertical hash marks and fix the hash length calcs.

--- FootballPlayBook/app/src/main/java/org/josebur/footballplaybook/PlayView.java
+++ FootballPlayBook/app/src/main/java/org/josebur/footballplaybook/PlayView.java
@@ -78,18 +78,24 @@ public class PlayView extends SurfaceView implements SurfaceHolder.Callback {
         private void doDraw(Canvas c) {
             c.drawRGB(0, 100, 0);
 
-            float oneYard = _canvasHeight / 30.0f;
-            for (int i = 5; i < 30; i += 5) {
+            float oneYard = _canvasHeight / 20.0f;
+            for (int i = 5; i < 20; i += 5) {
                 drawFiveYardIncrementLine(c, oneYard * i);
             }
 
             float oneFoot = _canvasWidth / 160.0f;
             float oneInch = oneFoot / 12.0f;
 
-            for (int i = 0; i < 30; i++) {
-                if (i % 5 == 0) continue;
-
-                drawHashMark(c, oneFoot, oneInch, oneYard, oneYard * i);
+            for (int i = 0; i < 20; i++) {
+                if (i % 5 == 0) {
+                    float leftHash = oneFoot * 70.0f + oneInch * 9.0f;
+                    float rightHash = _canvasWidth - (oneFoot * 70.0f + oneInch * 9.0f);
+                    float hashLength = oneYard * (1.0f/3.0f);
+                    drawVerticalHashMark(c, oneYard, leftHash + hashLength, rightHash - hashLength, oneYard * i);
+                }
+                else {
+                    drawHashMark(c, oneFoot, oneInch, oneYard, oneYard * i);
+                }
             }
         }
 
@@ -97,17 +103,26 @@ public class PlayView extends SurfaceView implements SurfaceHolder.Callback {
             c.drawLine(0, y, _canvasWidth, y, _yardLinePaint);
         }
 
+        private void drawVerticalHashMark(Canvas c, float oneYard, float leftHash, float rightHash, float y) {
+            float hashLength = oneYard * (1.0f/3.0f);
+
+            c.drawLine(leftHash, y - hashLength * 0.5f, leftHash, y + hashLength * 0.5f, _yardLinePaint);
+            c.drawLine(rightHash, y -  hashLength * 0.5f, rightHash, y + hashLength * 0.5f, _yardLinePaint);
+        }
+
         private void drawHashMark(Canvas c, float oneFoot, float oneInch, float oneYard, float y) {
+
+            float hashLength = oneYard * (1.0f/3.0f);
 
             float leftSideline = 6.0f * oneInch;
             float rightSideline = _canvasWidth - 6.0f * oneInch;
-            c.drawLine(leftSideline, y, leftSideline + oneYard * 0.5f, y, _yardLinePaint);
-            c.drawLine(rightSideline - oneYard * 0.5f, y, rightSideline, y, _yardLinePaint);
+            c.drawLine(leftSideline, y, leftSideline + hashLength, y, _yardLinePaint);
+            c.drawLine(rightSideline - hashLength, y, rightSideline, y, _yardLinePaint);
 
             float leftHash = oneFoot * 70.0f + oneInch * 9.0f;
             float rightHash = _canvasWidth - (oneFoot * 70.0f + oneInch * 9.0f);
-            c.drawLine(leftHash, y, leftHash + oneYard * 0.5f, y, _yardLinePaint);
-            c.drawLine(rightHash - oneYard * 0.5f, y, rightHash, y, _yardLinePaint);
+            c.drawLine(leftHash, y, leftHash + hashLength, y, _yardLinePaint);
+            c.drawLine(rightHash - hashLength, y, rightHash, y, _yardLinePaint);
         }
     }
 
