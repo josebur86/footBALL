8dfe59dd077ea0433f96c52b2e291f5b1453b545
Joe Burns <josebur86@gmail.com>
2015-01-27 07:18:12 -0600

    Add a gridiron business logic module and tests.

---
+++ FootballPlaybook3/gridiron/src/main/java/org/josebur/libraries/Player.java
@@ -0,0 +1,5 @@
+package org.josebur.libraries;
+
+public class Player {
+
+}
---
+++ FootballPlaybook3/gridiron/src/test/java/org/josebur/libraries/PlayerTest.java
@@ -0,0 +1,12 @@
+package org.josebur.libraries;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PlayerTest {
+    @Test
+    public void helloTest() {
+        assertTrue(true);
+    }
+}
