1ec872803041fd322114d992371a0f572716cf57
Joe Burns <josebur86@gmail.com>
2015-01-27 21:38:26 -0600

    Players have a label and optional position.

--- FootballPlaybook3/gridiron/src/main/java/org/josebur/libraries/Player.java
+++ FootballPlaybook3/gridiron/src/main/java/org/josebur/libraries/Player.java
@@ -2,4 +2,53 @@ package org.josebur.libraries;
 
 public class Player {
 
+    private final String _label;
+    private float _feetX;
+    private float _feetY;
+
+    private Player(Builder builder) {
+        _label = builder._label;
+        _feetX = builder._feetX;
+        _feetY = builder._feetY;
+    }
+
+    public String label() {
+        return _label;
+    }
+
+    public float feetX() {
+        return _feetX;
+    }
+
+    public float feetY() {
+        return _feetY;
+    }
+
+    public static class Builder {
+
+        // Required Fields
+        private final String _label;
+
+        // Optional Fields
+        private float _feetX = 0.f;
+        private float _feetY = 0.f;
+
+        public Builder(String label) {
+            _label = label;
+        }
+
+        public Builder feetX(float x) {
+            _feetX = x;
+            return this;
+        }
+
+        public Builder feetY(float y) {
+            _feetY = y;
+            return this;
+        }
+
+        public Player build() {
+            return new Player(this);
+        }
+    }
 }
--- FootballPlaybook3/gridiron/src/test/java/org/josebur/libraries/PlayerTest.java
+++ FootballPlaybook3/gridiron/src/test/java/org/josebur/libraries/PlayerTest.java
@@ -5,8 +5,39 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class PlayerTest {
+
+    private final float _feetDelta = 0.001f;
+
     @Test
-    public void helloTest() {
-        assertTrue(true);
+    public void builder_NameConstructor_PlayerHasTheSameLabel() {
+        String label = "Charlie";
+        Player p = new Player.Builder(label).build();
+
+        assertEquals(label, p.label());
+    }
+
+    @Test
+    public void builder_NameConstructor_PlayerHasZeroPosition() {
+        String label = "Charlie";
+        Player p = new Player.Builder(label).build();
+
+        assertEquals(0.f, p.feetX(), _feetDelta);
+        assertEquals(0.f, p.feetY(), _feetDelta);
+    }
+
+    @Test
+    public void builder_xFeet_PlayerHasLabelAndXPosition() {
+        String label = "Charlie";
+        Player p = new Player.Builder(label).feetX(10.f).build();
+
+        assertEquals(10.f, p.feetX(), _feetDelta);
+    }
+
+    @Test
+    public void builder_yFeet_PlayerHasLabelAndYPosition() {
+        String label = "Charlie";
+        Player p = new Player.Builder(label).feetY(15.f).build();
+
+        assertEquals(15.f, p.feetY(), _feetDelta);
     }
 }
